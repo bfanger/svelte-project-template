6b9e6fded18eb3f33de36b7231aa027352a26208
Bob Fanger <b.fanger@wearetriple.com>
2022-04-03 15:33:20 +0200

    feat: Experimental sourcemaps in dev

--- svelte.config.js
+++ svelte.config.js
@@ -3,9 +3,14 @@ import adapter from "@sveltejs/adapter-static";
 
 /** @type {import('@sveltejs/kit').Config} */
 export default {
-  preprocess: preprocess(),
+  preprocess: preprocess({ sourceMap: true }),
   kit: {
     prerender: { default: true },
     adapter: adapter(),
+    vite: {
+      css: {
+        devSourcemap: true, // Experimental and usually the wrong line, but the filename info is very useful.
+      },
+    },
   },
 };
