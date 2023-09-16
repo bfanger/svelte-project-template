8d8ca8338be720d8233e8eeaf45e9d5b3e05ce69
Bob Fanger <b.fanger@wearetriple.com>
2023-09-16 10:36:52 +0200

    feat: Improved buildUrl typing

--- src/utils/buildUrl.ts
+++ src/utils/buildUrl.ts
@@ -1,16 +1,27 @@
 /**
+ * ExtractPathParams<"/posts/[postId]/comments/[commentId]"> resolves to ["postId", "commentId"]
+ */
+type ExtractPathParams<T> = T extends `${string}[${infer Match}]${infer Rest}`
+  ? [Match, ...ExtractPathParams<Rest>]
+  : [];
+
+/**
+ * PathParams<"/posts/[postId]"> resolves to { postId: string | number, [key:string]: string | number }
+ */
+type Params<T> = Record<ExtractPathParams<T>[number], string | number> & {
+  [key: string]: string | number;
+};
+
+/**
  * Inject the params into the url and add the remaining params as search/query params
  *
  * Usage:
- *   buildUrl("/users/[id]", { id: 123, tab: "settings" }); // returns '/users/123?tab=settings
+ *   buildUrl("/users/[id]", { id: 123, tab: "settings" }); // returns "/users/123?tab=settings"
  */
-export default function buildUrl(
-  path: string,
-  params: Record<string, string | number>,
-) {
+export default function buildUrl<T extends string>(path: T, params: Params<T>) {
   const search = new URLSearchParams();
   let hasSearch = false;
-  let interpolatedPath = path;
+  let interpolatedPath = path as string;
   for (const [param, value] of Object.entries(params)) {
     const replaced = interpolatedPath.replace(`[${param}]`, value as string);
     if (replaced !== interpolatedPath) {
