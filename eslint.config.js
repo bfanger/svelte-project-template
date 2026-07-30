6e0709278ed36be3d6041eb5a8d5353c3f4cd243
Bob Fanger <bfanger@gmail.com>
2026-07-30 13:10:40 +0200

    feat: Improve compat with running dev inside docker

--- eslint.config.js
+++ eslint.config.js
@@ -14,6 +14,7 @@ export default defineConfig(
       ".vercel",
       "build",
       "node_modules",
+      ".pnpm-store",
       "dist",
       "vite.config.ts.timestamp-*.mjs",
     ],
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { existsSync } from "node:fs";
 import { varlockVitePlugin } from "@varlock/vite-integration";
 import { sveltekit } from "@sveltejs/kit/vite";
 import tailwindcss from "@tailwindcss/vite";
@@ -7,4 +8,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [varlockVitePlugin(), devtoolsJson(), sveltekit(), tailwindcss()],
   css: { devSourcemap: true },
+  server: existsSync("/.dockerenv")
+    ? { host: true, watch: { usePolling: true, interval: 1000 } }
+    : undefined,
 });
